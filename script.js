19c02bde7afbb72e05cb22c37481dd39c6373720
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-05-31 15:51:17 +0200

    Remove previous change as it is not relevant

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ function playRound(humanChoice, computerChoice) {
   const lossStr = "You lose!";
   const winStr = "You win!";
   const drawStr = "It's a draw!";
-  humanChoice = getHumanChoice();
-  computerChoice = getComputerChoice();
 
   if (humanChoice === "rock") {
     switch (computerChoice) {
@@ -59,3 +57,5 @@ function playRound(humanChoice, computerChoice) {
     }
   }
 }
+
+playRound()
